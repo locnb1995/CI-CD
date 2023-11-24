fa73689b6344c3f55521167186fa7b8db63424f1
Nguyen Loc <nguyenloc@192.168.2.11>
2023-11-24 23:46:56 +0700

    init project and jenkinsfile

---
+++ demo/src/main/java/com/stormaws/demo/ctrl/AppController.java
@@ -0,0 +1,27 @@
+package com.stormaws.demo.ctrl;
+
+import com.stormaws.demo.model.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+
+    @RequestMapping(method = RequestMethod.GET
+            , value = "index"
+            , consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> index(){
+        Person person = new Person();
+        person.setName("Loc");
+        person.setAge(28);
+        person.setHometown("Thai Binh");
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+}
---
+++ demo/src/main/java/com/stormaws/demo/model/Person.java
@@ -0,0 +1,34 @@
+package com.stormaws.demo.model;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    private String hometown;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+}
