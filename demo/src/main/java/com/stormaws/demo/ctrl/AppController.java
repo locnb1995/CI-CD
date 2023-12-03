3ce94267d900ad11067b00b3a95d11c780d3cac6
locnb1995 <baoloc.hus@gmail.com>
2023-12-03 22:33:48 +0700

    Update AppController.java

--- demo/src/main/java/com/stormaws/demo/ctrl/AppController.java
+++ demo/src/main/java/com/stormaws/demo/ctrl/AppController.java
@@ -18,7 +18,7 @@ public class AppController {
             , produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Person> index(){
         Person person = new Person();
-        person.setName("Locnb1");
+        person.setName("Locnb222");
         person.setAge(29);
         person.setHometown("Nguyen Trai, Thanh Xuan, Ha Noi");
         return new ResponseEntity<>(person, HttpStatus.OK);
